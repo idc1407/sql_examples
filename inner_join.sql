937ac85b5722b40ccbd7761e4ddca1f3fbe81555
Ivan Dcosta <idc1407@hotmail.com>
2021-07-09 19:47:16 +0530

    i090720210947

---
+++ inner_join.sql
@@ -0,0 +1,34 @@
+--inner join (records which exists in child tables will be queried) ;
+
+
+declare @emp_mst table (emp_id int, emp_name varchar(100)) ;
+declare @emp_trn table (emp_id int, mth int, yy int, net_salary int) ;
+
+insert into @emp_mst (emp_id, emp_name) 
+select 1, 'emp_01'
+union all 
+select 2, 'emp_02'
+union all 
+select 3, 'emp_03' ;
+
+insert into @emp_trn (emp_id, mth, yy, net_salary) 
+select 1, 4, 2020, 900
+union all 
+select 1, 5, 2020, 1000
+union all 
+select 2, 4, 2020, 1900 ;
+
+
+select 
+a.emp_id,
+a.emp_name,
+b.mth,
+b.yy,
+b.net_salary 
+from @emp_mst a
+inner join @emp_trn b on a.emp_id = b.emp_id ;
+
+
+
+
+
